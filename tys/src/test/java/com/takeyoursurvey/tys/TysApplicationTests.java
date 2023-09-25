a836a38100bb5cca830a783552860f544dfd2a6c
leejh-96 <sbxl12@naver.com>
2023-09-26 00:47:29 +0900

    [test]

--- tys/src/test/java/com/takeyoursurvey/tys/TysApplicationTests.java
+++ tys/src/test/java/com/takeyoursurvey/tys/TysApplicationTests.java
@@ -3,10 +3,10 @@ package com.takeyoursurvey.tys;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class TysApplicationTests {
 
-//	@Test
+	@Test
 	void contextLoads() {
 	}
 
