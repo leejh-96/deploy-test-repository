3387ab57ff5ec116c8893f6fae55e403164d4c88
leejh-96 <sbxl12@naver.com>
2023-09-25 16:01:52 +0900

    [test]

---
+++ tys/src/main/java/com/takeyoursurvey/tys/TysApplication.java
@@ -0,0 +1,13 @@
+package com.takeyoursurvey.tys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TysApplication.class, args);
+	}
+
+}
---
+++ tys/src/test/java/com/takeyoursurvey/tys/TysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takeyoursurvey.tys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
