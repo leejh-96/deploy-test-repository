23a3a7bcfd1b83d32148248d49a1e2865eba4da1
leejh-96 <sbxl12@naver.com>
2023-09-26 17:24:35 +0900

    [test]

--- tys/src/main/java/com/takeyoursurvey/tys/controller/HelloController.java
+++ tys/src/main/java/com/takeyoursurvey/tys/controller/HelloController.java
@@ -1,26 +1,12 @@
 package com.takeyoursurvey.tys.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @Value("${my-env.first}")
-    private String first;
 
-    @Value("${my-env.second}")
-    private Integer second;
-
-
-    @GetMapping("/hello")
-    String ping() {
-        return String.format(
-                "안녕! 설정된 정보는 FIRST = [%s], SECOND= [%d] !",
-                first,
-                second);
-    }
 
 }
